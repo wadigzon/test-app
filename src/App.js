453af5530255fc9fff8463c674b306e51e1e8113
Wadigzon Diaz-Wong <wadigzon@gmail.com>
2024-05-12 14:11:53 -0700

    .- added lesson

--- src/App.js
+++ src/App.js
@@ -5,10 +5,32 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1>Hello Conner</h1>
-      <h1>Hello Wadi</h1>
+      <Hello name= "Wadi" />
+      <Hello name= "Clement" />
+      <Hello />
+      
+      {/* component composition: having children - component inside of component */}
+      <Comment username="Wadi" time={(new Date()).toString()}>
+        <h1>Hello World</h1>
+        <p>This is a comment</p>
+      </Comment>
+
     </>
   );
+} 
+
+
+function Hello({name = "User"}) {
+  return <h1>Hello {name}</h1>
+}
+
+function Comment({username, time, children}) {
+  return (
+    <section>
+      <p>{username} commented at {time} </p>
+      {children}
+    </section>
+  )
 }
 
 export default App;
