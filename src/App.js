ff975d8548932240a15f74cdd67c7ad6b3a796d1
Wadigzon Diaz-Wong <wadigzon@gmail.com>
2024-05-12 13:40:46 -0700

    .- added initial changes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,10 @@ import './App.css';
 
 function App() {
   return (
-    <Comment username="Conner" time={(new Date()).toString()}>
-      <h1>Hello World</h1>
-    </Comment>
-  );
-}
-
-function Comment({children, username, time}) {
-  return (
-    <section>
-    <p>{username} commented at {time} </p>
-    {children}
-  </section>
+    <>
+      <h1>Hello Conner</h1>
+      <h1>Hello Wadi</h1>
+    </>
   );
 }
 
