7f3ef98097b179375d1cd1034cdae1f3bbf68699
Wadigzon Diaz-Wong <wadigzon@gmail.com>
2024-05-12 14:15:11 -0700

    .- replaced an almost blank state

--- src/App.js
+++ src/App.js
@@ -4,19 +4,10 @@ import './App.css';
 
 function App() {
   return (
-    <Comment username="Conner" time={(new Date()).toString()}>
-      <h1>Hello World</h1>
-    </Comment>
+    <h1>Hello World!</h1>
   );
 }
 
-function Comment({children, username, time}) {
-  return (
-    <section>
-    <p>{username} commented at {time} </p>
-    {children}
-  </section>
-  );
-}
+
 
 export default App;
