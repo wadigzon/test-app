fb11995922ac1daf47db4e3cae23bc97f22d3225
Wadigzon Diaz-Wong <wadigzon@gmail.com>
2024-05-12 21:43:01 -0700

    .- added initial content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 export default function App() {
+  const [seconds, setSeconds] = useState(0);
+
+  const startTimer = () => {
+    setInterval(() => {
+      setSeconds(currSeconds => currSeconds + 1);
+    }, 1000);
+  }
+
+  const stopTimer = () => {
+
+  };
+
   return (
-    <h1>Hello World!</h1>
+    <>
+      <button onClick={startTimer}>Start</button>
+      <button onClick={stopTimer}>Stop</button>
+      <p>seconds: {seconds} </p>
+    </>
   );
 }
