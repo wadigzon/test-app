f10dd8a145f8ccf8a0c61d4d5a7203ba543f1ad1
Wadigzon Diaz-Wong <wadigzon@gmail.com>
2024-05-13 00:47:17 -0700

    .- added starter code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { useState, useRef, useEffect } from "react";
+
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState(0);
+
+  const prevCount = useRef();
+  useEffect(() => {
+    prevCount.current = count;
+  }, [count]);
+  
+  const prevText = useRef();
+  useEffect(() => {
+    prevText.current = text;
+  }, [text]);
+
   return (
-    <h1>Hello World!</h1>
+    <>
+      <button onClick={() => setCount(count+1)}>Increment</button>
+      <p>Count: {count}</p>
+      <p>Previous render count: {prevCount.current}</p>
+
+      <input
+        value={text}
+        onChange={(event) => setText(event.target.value)} />
+        <p>Previous render text: {prevText.current}</p>
+    </>
   );
 }
