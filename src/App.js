87a78ef0eb1aaa5b802178ff684826d8b2444d10
Wadigzon Diaz-Wong <wadigzon@gmail.com>
2024-05-12 22:06:35 -0700

    .- added content

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-import { useState } from "react";
+import { useState, useRef, forwardRef } from "react";
 
-export default function App() {
+export default function App(){
+  const inputRef = useRef(null);
+  const focusInput = () => {
+    inputRef.current.focus();
+  }
+
+  return (
+    <>
+      {/* <input ref={inputRef} /> */}
+      <MyInput ref = {inputRef} />
+      <MyInput ref = {handleRef} />
+      <button onClick={focusInput} >Focus</button>
+    </>
+  );
+}
+
+const MyInput = forwardRef(
+  function (props, ref) {
+    return <input ref={ref} {...props} style={{color: 'red'}} />
+  }
+)
+
+function handleRef(domNode) {
+  console.log(domNode);
+}
+
+// shows how useRef replaces useState with no use of setter function
+function App1() {
   const [seconds, setSeconds] = useState(0);
+  const timerId = useRef(null);
+  // const [timerId, setTimerId] = useState({
+  //  current: null
+  // })
 
   const startTimer = () => {
-    setInterval(() => {
+    // so we keep the current object and do not cause re-render
+    // note that setTimerId is not being used: not efficient code
+    timerId.current = setInterval(() => {
       setSeconds(currSeconds => currSeconds + 1);
     }, 1000);
   }
 
   const stopTimer = () => {
-
+    clearInterval(timerId.current);
   };
 
   return (
@@ -21,3 +54,4 @@ export default function App() {
     </>
   );
 }
+
