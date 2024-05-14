ad9c5847d3835d71770e658c5650c1cd4b9ff801
Wadigzon Diaz-Wong <wadigzon@gmail.com>
2024-05-13 17:18:12 -0700

    .- added starter code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,49 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
+  return (  
+    <>
+      <Counter startingCount={10} />
+      <Counter />
+    </>
+  );
+}
+
+function Counter({startingCount = 0}) {
+  const [count, setCount] = useState(startingCount);
+
+  useEffect(() => {
+    console.log('on 1')
+    return () => {
+      console.log('unmounted1')
+    }
+
+  }, [count]);
+
+  useEffect(() => {
+    console.log('on 2')
+    return () => {
+      console.log('unmounted2')
+    }
+
+  },[]);
+
+
+  useEffect(() => {
+    console.log('on 3');
+    return () => {
+      console.log('unmounted3')
+    }
+    
+  });
+
+
   return (
-    <h1>Hello World!</h1>
+    <>
+      <button onClick={() => setCount(count + 1)}
+      >Increment
+      </button>
+      <p>Count: {count}</p>
+    </>
   );
 }
