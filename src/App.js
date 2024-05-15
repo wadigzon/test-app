8cdab14c3851ea9588397206c3d976f85e0861d3
Wadigzon Diaz-Wong <wadigzon@gmail.com>
2024-05-14 21:33:07 -0700

    .- initial content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 export default function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>Hello World</h1>
+      <Buggy />
+    </>
   );
+}
+
+function Buggy() {
+  throw new Error('Error');
+  return <h1>Buggy</h1>
 }
