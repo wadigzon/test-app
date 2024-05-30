3d3cb7ea1e51112764e9196d2d9d51528cb3f137
Wadigzon Diaz-Wong <wadigzon@gmail.com>
2024-05-29 22:07:16 -0700

    .- added content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { /*useEffect,*/ useState, Profiler, useDebugValue } from "react";
+
+let renderCount = 0;
+
 export default function App() {
+  //useEffect(() =>{
+    renderCount++;
+  //});
+  console.log('rendering');
+
   return (
-    <h1>Hello World!</h1>
+    <Profiler id="App" onRender={() => console.log('comit')}>
+      <Counter initialValue={5}/>
+      <Counter />
+      <p>Render Count: {renderCount} </p>
+    </Profiler>
   );
+}
+
+function Counter({initialValue}) {
+  const [count, setCount] = useMyState(initialValue);
+
+  const startTime = Date.now();
+  while (new Date() - startTime < 500) {}
+
+  return (
+    <>
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+      <p>Count: {count}</p>
+    </>
+  )
+}
+
+function useMyState(initialValue = 0) {
+  useDebugValue();
+  return useState(initialValue);
 }
